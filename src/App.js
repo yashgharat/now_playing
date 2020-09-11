d364e29410b1b1cc11e88da27c315d721bf97cc2
Yash Gharat <yashgharat12@gmail.com>
2020-09-11 16:40:04 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var client_id = 'a2904de6730840a199d8f57925d9d68c'; // Your client id
+var client_secret = '31713129f3bc4b49b9b47311e90b9ca0'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Or Your redirect uri
+
+const spotifyApi = new SpotifyWebApi();
+
+class App extends React.Component {
+  render () {
+    return(
+      <div className="App">
+        <a href='http://localhost:3000' > Login to Spotify </a>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ProgressBar.module.css';
+
+const ProgressBar = () => (
+  <div className={styles.ProgressBar}>
+    ProgressBar Component
+  </div>
+);
+
+ProgressBar.propTypes = {};
+
+ProgressBar.defaultProps = {};
+
+export default ProgressBar;
