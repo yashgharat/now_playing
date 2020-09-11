b07372b810d0fdaeab02dfc9a8d92f5fd296cc09
Yash Gharat <yashgharat12@gmail.com>
2020-09-11 17:41:40 -0400

    now-playing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,95 @@
 import React from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 
-var client_id = 'a2904de6730840a199d8f57925d9d68c'; // Your client id
-var client_secret = '31713129f3bc4b49b9b47311e90b9ca0'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Or Your redirect uri
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const client_id = 'a2904de6730840a199d8f57925d9d68c'; // Your client id
+const client_secret = '31713129f3bc4b49b9b47311e90b9ca0'; // Your secret
+const redirectUri = "http://localhost:3000"; // Or Your redirect uri
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
 
 const spotifyApi = new SpotifyWebApi();
 
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = ""
+
 class App extends React.Component {
-  render () {
-    return(
-      <div className="App">
-        <a href='http://localhost:3000' > Login to Spotify </a>
-      </div>
-    )
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+    }
   }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
+
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+        {!this.state.token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}client_id=${client_id}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {this.state.token}
+        </header>
+      </div>
+    );
+    }
+  
 }
 
 export default App;
