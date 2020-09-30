03ee827f7afbe302d618470ecbad152597684f7c
Yash Gharat <yashgharat12@gmail.com>
2020-09-29 22:49:48 -0400

    auth working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { Button } from 'antd';
 import React from 'react';
+import axios from 'axios';
+import logo from "./logo.svg";
 import SpotifyWebApi from 'spotify-web-api-js';
 
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+const base_url = 'https://accounts.spotify.com/';
+const queryString = require('query-string');
+
 
 const client_id = 'a2904de6730840a199d8f57925d9d68c'; // Your client id
 const client_secret = '31713129f3bc4b49b9b47311e90b9ca0'; // Your secret
-const redirectUri = "http://localhost:3000"; // Or Your redirect uri
+const redirect_uri = "http://localhost:3000"; // Or Your redirect uri
 const scopes = [
   "user-read-currently-playing",
   "user-read-playback-state",
 ];
-
-const spotifyApi = new SpotifyWebApi();
 
 const hash = window.location.hash
   .substring(1)
@@ -23,44 +26,10 @@ const hash = window.location.hash
     }
     return initial;
   }, {});
-window.location.hash = ""
+
+window.location.hash = "";
 
 class App extends React.Component {
-  constructor(){
-    super();
-    const params = this.getHashParams();
-    const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
-    }
-    this.state = {
-      loggedIn: token ? true : false,
-    }
-  }
-  getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-    return hashParams;
-  }
-
-  getNowPlaying(){
-    spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
-            }
-        });
-      })
-  }
-
   componentDidMount() {
     // Set token
     let _token = hash.access_token;
@@ -69,6 +38,7 @@ class App extends React.Component {
       this.setState({
         token: _token
       });
+      console.log(_token);
     }
   }
 
@@ -76,20 +46,25 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-        {!this.state.token && (
-          <a
-            className="btn btn--loginApp-link"
-            href={`${authEndpoint}client_id=${client_id}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-          >
-            Login to Spotify
-          </a>
-        )}
-        {this.state.token}
+          <Button color="green" onClick={this.loginWithSpotify}>
+            Login with Spotify
+          </Button>
         </header>
       </div>
     );
     }
   
-}
+    loginWithSpotify() {
+      var url = base_url + 'authorize?';
+
+      window.location.href = url +
+      queryString.stringify({
+        response_type: 'token',
+        client_id,
+        redirect_uri: redirect_uri,
+      });
+    }
+  }
+
 
 export default App;
